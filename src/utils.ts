04a520ba7820e3587b558dfa2f8fa6fde4739b0d
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-05-26 03:36:36 -0300

    docs: Add detailed documentation for utility functions

--- src/utils.ts
+++ src/utils.ts
@@ -43,6 +43,17 @@ export const sortClassString = (
 	return classArray.join(options.replacement || ' ').trim();
 };
 
+/**
+ * Sorts an array of CSS classes based on a specific sorting order and an
+ * optional setting to include custom classes.
+ * @param {string[]} classArray - The array of CSS classes to be sorted.
+ * @param {string[]} sortOrder - The array defining the desired sorting order
+ * for the CSS classes.
+ * @param {boolean} shouldPrependCustomClasses - Indicates whether classes not
+ * in the sortOrder should be added at the beginning (true) or at the end
+ * (false) of the sorted array.
+ * @returns {string[]} A new sorted array of CSS classes.
+*/
 const sortClassArray = (
 	classArray: string[],
 	sortOrder: string[],
@@ -59,10 +70,24 @@ const sortClassArray = (
 	), // prepend the classes that were not in the sort order if configured this way
 ];
 
+/**
+ * The `removeDuplicates` function takes an array of strings and returns a new
+ * array with duplicate values removed.
+ * @param {string[]} classArray - An array of strings containing class names.
+ */
 const removeDuplicates = (classArray: string[]): string[] => [
 	...new Set(classArray),
 ];
 
+/**
+ * The function `isArrayOfStrings` checks if a value is an array containing only
+ * string elements.
+ * @param {unknown} value - The `isArrayOfStrings` function checks if the
+ * `value` is an array of strings by verifying if it is an array and if every
+ * element in the array is of type `string`.
+ * @returns The function `isArrayOfStrings` returns a boolean value indicating
+ * whether the input `value` is an array of strings or not.
+ */
 function isArrayOfStrings(value: unknown): value is string[] {
 	return (
 		Array.isArray(value) && value.every((item) => typeof item === 'string')
@@ -75,6 +100,12 @@ export type Matcher = {
 	replacement?: string;
 };
 
+/**
+ * The function `buildMatcher` takes a `LangConfig` value and returns a `Matcher`
+ * object based on the type and properties of the input value.
+ * @param {LangConfig} value - The input configuration to build a matcher from.
+ * @returns The function `buildMatcher` returns an object `Matcher`.
+ */
 function buildMatcher(value: LangConfig): Matcher {
 	if (typeof value === 'string') {
 		return {
@@ -105,6 +136,13 @@ function buildMatcher(value: LangConfig): Matcher {
 	}
 }
 
+/**
+ * The function `buildMatchers` takes a value or an array of values, checks their
+ * type, and returns an array of Matcher objects.
+ * @param {LangConfig | LangConfig[]} value - The input configuration(s) to
+ * build matchers from.
+ * @returns An array of Matcher objects is being returned.
+ */
 export function buildMatchers(value: LangConfig | LangConfig[]): Matcher[] {
 	if (value == undefined) {
 		return [];
@@ -118,6 +156,23 @@ export function buildMatchers(value: LangConfig | LangConfig[]): Matcher[] {
 	return [buildMatcher(value)];
 }
 
+/**
+ * The function `getTextMatch` takes an array of regular expressions, a text
+ * string, a callback function, and an optional starting position, and
+ * recursively matches the regular expressions in the text string, calling the
+ * callback function with the matched text and its position.
+ * @param {RegExp[]} regexes - it will be used to search for matches in the
+ * `text` parameter.
+ * @param {string} text - which you want to search for matches based on the
+ * provided regular expressions.
+ * @param callback - is a function that takes two arguments: `text` (a string)
+ * and `startPosition` (a number). This callback function is called for each
+ * match found in the `text` based on the provided regular expressions.
+ * @param {number} [startPosition=0] - represents the starting index in the
+ * `text` string from which the search for matches should begin. By default, it
+ * is set to 0 if no value is provided when calling the function. This parameter
+ * allows you to specify an initial position
+ */
 export function getTextMatch(
 	regexes: RegExp[],
 	text: string,
